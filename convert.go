aa7076ff7131d4b9acdab615c98277def9bb0548
prr123 <priemen@gmail.com>
2022-06-09 10:30:49 +0200

    initial commit

---
+++ convert.go
@@ -0,0 +1,47 @@
+// convert .go
+// author pr azul software
+// date: 9. June 2022
+// update:
+// copyright prr, azul softwre
+// license: see github
+//
+
+package main
+
+import (
+	"fmt"
+	"os"
+	)
+
+
+func main () {
+// function that parses a json structure and create a go structure
+
+	var inpfilNam, gofilNam string
+
+	numArg := len(os.Args)
+
+	switch numArg {
+	case 1:
+		fmt.Println("insufficient arguments!")
+		fmt.Printf("Usage is:\n  %s infile gofile!\n", os.Args[0])
+		os.Exit(1)
+
+	case 2:
+		inpfilNam = os.Args[1]
+	case 3:
+		inpfilNam = os.Args[1]
+		gofilNam = os.Args[2]
+
+	default:
+		fmt.Printf("too many arguments in cmd line: %d!", numArg)
+		fmt.Printf("Usage is:\n  %s infile gofile!\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	fmt.Println("******** Convert ********")
+	fmt.Printf("input file: %s\n", inpfilNam)
+	fmt.Printf("go file:    %s\n", gofilNam)
+
+	fmt.Println("**** Convert Success ****")
+}
